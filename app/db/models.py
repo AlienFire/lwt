21d63aa7bce820accb7c00de0d4d63cbc46f10de
Ogneva.A.C <ognevaalena22@gmail.com>
2024-04-11 10:02:21 +0300

    Added db

---
+++ app/db/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+
+from datetime import datetime
+
+Base = declarative_base()
+
+
+class Content(Base):
+    __tablename__ = "content"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), unique=True)
+    created_at = Column(DateTime, default=datetime.now, server_default=)
