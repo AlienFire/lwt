ae58bc520b7d4076a5de8321f0cc60e55e54da15
Ogneva.A.C <ognevaalena22@gmail.com>
2024-06-06 12:01:00 +0300

    Added content authorization

--- app/api/content.py
+++ app/api/content.py
@@ -4,6 +4,8 @@ from app.di.filters import get_content_filter
 from app.di.services import get_content_service
 from app.schema import ContentInput, ContentOut, ContentFilterEntity
 from app.services import ContentService
+from app.db.models import User
+from app.di.auth import get_auth_user
 
 content_router = APIRouter()
 
@@ -12,6 +14,7 @@ content_router = APIRouter()
 async def get_all_content(
     filter_: ContentFilterEntity = Depends(get_content_filter),
     service: ContentService = Depends(get_content_service),
+    auth_user: User = Depends(get_auth_user),
 ) -> list[ContentOut]:
     results = await service.get_contents(filter_=filter_)
     return results
@@ -21,6 +24,7 @@ async def get_all_content(
 async def get_one_content(
     id: int,
     service: ContentService = Depends(get_content_service),
+    auth_user: User = Depends(get_auth_user),
 ) -> ContentOut:
     result = await service.get_content(ident=id)
     return ContentOut.model_validate(result, from_attributes=True)
@@ -46,6 +50,7 @@ async def change_content(
     id: int,
     payload: ContentInput,
     service: ContentService = Depends(get_content_service),
+    auth_user: User = Depends(get_auth_user),
 ):
     update_content = await service.update_content(id=id, name=payload.name)
     return ContentOut.model_validate(update_content, from_attributes=True)
@@ -55,6 +60,7 @@ async def change_content(
 async def delete_content(
     id: int,
     service: ContentService = Depends(get_content_service),
+    auth_user: User = Depends(get_auth_user),
 ) -> None:
     await service.delete_content(id=id)
     return None
